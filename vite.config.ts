9e0d7eacb5e8ea2c609bcf2d7531670341364ce4
Daniel <deco2449584@gmail.com>
2025-02-26 14:07:38 -0500

    vite config 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Nombre de tu repositorio
-})
+  base: "/", // Nombre de tu repositorio
+  server: {
+    port: 3000,
+    host: true, // Permite conexiones desde otros dispositivos
+    open: true, // Abre el navegador automáticamente
+  },
+});
