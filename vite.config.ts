16209b61c1372fef95ea430078b583a0939d62ce
Deco2449584 <deco2449584@gmail.com>
2025-04-09 16:03:30 -0500

    Update vite.config.ts
    viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web/', // Nombre de tu repositorio
+  base: process.env.NODE_ENV === 'production' ? 'https://pralamb.com' : '/',
+  server: {
+    port: 3000,       // Puerto en el que se ejecuta el servidor
+    open: true,       // Abre el navegador automáticamente al iniciar el servidor
+    host: true,       // Escucha en todas las interfaces (0.0.0.0), permitiendo acceso desde otros dispositivos
+  },
 })
