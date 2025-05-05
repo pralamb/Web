cd89eff4dcee6e7456b311eb3b6d66a1f8759d2e
Daniel <dcaro@skgtecnologia.com>
2025-05-04 22:40:56 -0500

    modificacion al app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import { muiTheme } from "./config/theme";
 import { ROUTES } from "./config/constants";
+import { useEffect } from "react";
 
 // Pages
 import Home from "./pages/Home";
@@ -13,9 +14,25 @@ import Contact from "./pages/Contact";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
+function RouteHandler() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Solo redirigir si estamos en una URL directa sin hash
+    if (location.pathname !== '/' && !window.location.hash && !location.key) {
+      // Usar navigate para una transición suave sin recargar la página
+      navigate(location.pathname + location.search, { replace: true });
+    }
+  }, [location, navigate]);
+
+  return null;
+}
+
 function App() {
   return (
     <ThemeProvider theme={muiTheme}>
+      <RouteHandler />
       <Navbar />
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />} />
@@ -24,6 +41,7 @@ function App() {
         <Route path={ROUTES.BLOG} element={<Blog />} />
         <Route path={ROUTES.BLOG_POST} element={<BlogPost />} />
         <Route path={ROUTES.CONTACT} element={<Contact />} />
+        <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} />
       </Routes>
       <Footer />
     </ThemeProvider>
