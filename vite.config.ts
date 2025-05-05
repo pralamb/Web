bbf7c699a1d8fbde84e3802c3661daf6c136e9a5
Daniel <dcaro@skgtecnologia.com>
2025-05-04 22:30:22 -0500

    routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? 'https://pralamb.com' : '/',
+  base: "./",
   server: {
-    port: 3000,       // Puerto en el que se ejecuta el servidor
-    open: true,       // Abre el navegador automáticamente al iniciar el servidor
-    host: true,       // Escucha en todas las interfaces (0.0.0.0), permitiendo acceso desde otros dispositivos
+    port: 3000, // Puerto en el que se ejecuta el servidor
+    open: true, // Abre el navegador automáticamente al iniciar el servidor
+    host: true, // Escucha en todas las interfaces (0.0.0.0), permitiendo acceso desde otros dispositivos
   },
-})
+});
